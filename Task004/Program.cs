8cb6c2b632b7ee5199c436d82e26b720124c0872
GeorgeSaltykov <sonofpeley@gmail.com>
2022-08-10 00:34:38 +0300

    Add Task 004

---
+++ Task004/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Программа выявления максимального числа из трех. Введите три числа.");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+if(max < secondNumber)
+{
+    max = thirdNumber;
+}
+else if(max < thirdNumber)
+{
+    max = thirdNumber;
+}
+Console.WriteLine($"Максимальное число: {max}");
