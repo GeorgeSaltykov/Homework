564c85b54bc73e2fd34d1c1c55045f1d840cbfbf
GeorgeSaltykov <sonofpeley@gmail.com>
2022-08-10 01:15:19 +0300

    Add Task008

---
+++ Task008/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Программа, которая выводит все четные числа до заданного числа. Введите число.");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count <= number)
+{
+    if(count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+}
