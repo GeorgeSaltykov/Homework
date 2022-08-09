f08708dd04481c12fd6ed981aa2b2df8e0cb2c6f
GeorgeSaltykov <sonofpeley@gmail.com>
2022-08-09 23:15:12 +0300

    Forgot about initial commit. So... Add Task 002

---
+++ Task001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Программа для сравнения чисел. Введите число, затем введите второе.");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumbeer = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+int min = secondNumbeer;
+if(firstNumber < secondNumbeer)
+{
+    max = secondNumbeer;
+    min = firstNumber;
+}
+Console.WriteLine($"Число {max} больше числа {min}");
