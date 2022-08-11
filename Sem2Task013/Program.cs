4fa493466648af9991522cb5cca07700cefbc02a
GeorgeSaltykov <sonofpeley@gmail.com>
2022-08-12 02:49:28 +0300

    Added Task 13 from second seminar homework

---
+++ Sem2Task013/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите целое число.");
+int someDigitNumber (int number)
+{
+    if(number > 0)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        if (number > 99)
+        {
+            number = number % 10;
+        }
+        else
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+    }
+    else if(number < 0)
+    {
+        while (number < -999)
+        {
+            number = number / 10;
+        }
+        if (number < -99)
+        {
+            number = number % 10;
+        }
+        else
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+    }
+    return number;
+}
+Console.WriteLine(someDigitNumber(Convert.ToInt32(Console.ReadLine())));
