34afb53baad387ca98bcc69b4b1b01454985340a
GeorgeSaltykov <sonofpeley@gmail.com>
2022-08-12 01:06:42 +0300

    Added Task 10 from homework after second seminar

---
+++ Sem2Task010/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите трехзначное число.");
+int Number = Convert.ToInt32(Console.ReadLine());
+int threeDigitNumber (int number)
+{
+    int secondNumber = number % 100 / 10;
+    return secondNumber;
+}
+Console.WriteLine($"Вторая цифра вашего числа: {threeDigitNumber(Number)}");
