9baa2dbc639eb38a3b6fcbb16e26a650ed3a86d8
GeorgeSaltykov <sonofpeley@gmail.com>
2022-08-12 11:55:23 +0300

    Added Task 15 from homework of second seminar

--- Sem2Task015/Program.cs
+++ Sem2Task015/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ввендите номер дня недели (от одного до семи).");
+string WeekDay (int number) {
+    string [] week = {"будний день", "будний день", "будний день", "будний день", "будний день", "выходной день", "выходной день", ""};
+    if (number > 0 && number < 8) 
+    {
+        number--;
+        return week [number];
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное число");
+        return week [7];
+    }
+
+}
+Console.WriteLine(WeekDay(Convert.ToInt32(Console.ReadLine())));
