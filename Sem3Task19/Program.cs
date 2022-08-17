5567a5c4dc7c4b47b9951d52d049731e1f93384c
GeorgeSaltykov <sonofpeley@gmail.com>
2022-08-18 00:27:17 +0300

    Add Task 19

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,60 @@
+﻿// Напишите метод, который принимает на вход шестизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+// строки использовать нельзя
+
+void palindrome(int userNumber)
+{
+    int first = userNumber / 100000;
+    int last = userNumber % 10;
+    if (userNumber > 99999 && userNumber < 1000000)
+    {
+        if (first == last)
+        {
+            first = userNumber / 10000 % 10;
+            last = userNumber / 10 % 10;
+        }
+        if (first == last)
+        {
+            first = userNumber / 1000 % 10;
+            last = userNumber / 100 % 10;
+        }
+        if (first == last)
+        {
+            Console.WriteLine("Ваше число является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Не является полиндромом");
+        }
+    }
+    else if (userNumber < -99999 && userNumber > -1000000)
+    {
+        if (first == last)
+        {
+            first = userNumber / 10000 % 10;
+            last = userNumber / 10 % 10;
+        }
+        if (first == last)
+        {
+            first = userNumber / 1000 % 10;
+            last = userNumber / 100 % 10;
+        }
+        if (first == last)
+        {
+            Console.WriteLine("Ваше число является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Не является полиндромом");
+        }
+    }
+
+    else Console.WriteLine("Ваше число не соответствует требованиям");
+
+
+}
+
+Console.WriteLine("Введите шестизначное число");
+palindrome(Convert.ToInt32(Console.ReadLine()));
