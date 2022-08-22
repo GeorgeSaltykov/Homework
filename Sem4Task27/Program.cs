2149d6be562ae8bc846ee4aaf54e54e93c1d65fa
GeorgeSaltykov <sonofpeley@gmail.com>
2022-08-22 18:51:52 +0300

    Add Task 27

--- Sem4Task27/Program.cs
+++ Sem4Task27/Program.cs
@@ -3,39 +3,14 @@
 // 82 -> 10
 // 9012 -> 12
 
-// int sum(string number)
-// {
-//     int result = 0;
-//     for(int i = 0; i < number.Length; i++)
-//     {
-//         char[] stringToArray = new char[number.Length];
-//         stringToArray[i] = number[i];
-//         System.Console.WriteLine(stringToArray[i]);
-//         int charToInt = Convert.ToInt32(stringToArray[i]);
-//         System.Console.WriteLine(charToInt);
-//         result = result + charToInt;
-//     }
-//     return result;
-// }
-// System.Console.WriteLine(sum(Console.ReadLine()));
-
-int sum(string number)
+void sum(string number)
 {
-    int i = 0;
-    int[] result = new int[number.Length];
-    int sum = 0;
-    while (i < number.Length)
+    int result = 0;
+    for(int i = 0; i < number.Length; i++)
     {
-        char[] stringToArray = new char[number.Length];
-        stringToArray[i] = number[i];
-        result[i] = Convert.ToInt32(stringToArray[i]);
-        i++;
+        string stringTo = Convert.ToString(number[i]);
+        result = result + Convert.ToInt32(stringTo);
     }
-    for(int index = 0; index < result.Length; index++)
-    {
-        sum = sum + result[i];
-    }
-    return sum;
+    System.Console.WriteLine(result);
 }
-System.Console.WriteLine(sum(Console.ReadLine()));
-
+sum(Console.ReadLine());
