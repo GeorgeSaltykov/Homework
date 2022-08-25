642806a67675ca26e9447f4a597684cf8fe1cc00
GeorgeSaltykov <sonofpeley@gmail.com>
2022-08-25 16:18:02 +0300

    Add Task 36

---
+++ Sem5Task36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+float[] fillArray(int userNumber)
+{
+    float[] array = new float[userNumber];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-1000, 1001);
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+    return array;
+}
+
+float diff(float[] array)
+{
+    float min = array[0];
+    float max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(min > array[i]) min = array[i];
+        else if(max < array[i]) max = array[i];
+    }
+    return max - min;
+}
+System.Console.WriteLine("Введите размер массива");
+System.Console.WriteLine(diff(fillArray(Convert.ToInt32(Console.ReadLine()))));
