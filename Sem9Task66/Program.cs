09e1d6675c95d4b56aa225d248c56dd7d3254662
GeorgeSaltykov <sonofpeley@gmail.com>
2022-09-08 17:17:54 +0300

    Add Task 66

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int naturalNumbersSum(int M, int N)
+{
+    int result = M;
+    while (M < N)
+    {
+        result += M + 1;
+        M++;
+    }
+    return result;
+}
+System.Console.WriteLine(naturalNumbersSum(1, 15));
